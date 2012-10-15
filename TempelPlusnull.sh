990996149a34d41b1b6f04d7be552adef1bd84f5
Anton Litovtšenko <anton.litovtsenko@sk.ee>
2012-10-15 13:57:23 +0000

    TempelPlus 1.1.1 conf
    git-svn-id: https://arendus.sk.ee/svn/svnpki/Tempelplus/trunk@1882 984e8997-5708-0410-b877-c265616a6196

--- TempelPlusnull.sh
+++ TempelPlusnull.sh
@@ -5,10 +5,11 @@ echo "Starting TempelPlus from shell script"
 
 #Variables
 JAVA_HOME="/home/anton/bin/jdk1.6.0_32/jre/bin/java"
+﻿CLASSPATH=﻿﻿/home/anton/bin/jenkins/workspace/Tempel Plus on Fedora 12 x86/JDigiDoc/jdigidoc-r7422.jar
 #JAVA_HOME="java"
 ENCODING=UTF-8
 
 #Starts Run.jar, uses native libraries from existing java path and also from "linux" folder (needed for loading libpkcs11wrapper.so, which by default most probably may not belong to java lib path)
 #Commandline arguments are passed using syntax ${*+"$*"}) in order to handle embedded spaces properly (if needed)
 
-$JAVA_HOME -Dfile.encoding=$ENCODING -Xmx1024m -Djava.library.path="linux/32:$PATH" -jar Run.jar ${1+"$1"} ${2+"$2"} ${3+"$3"} ${4+"$4"} ${5+"$5"} ${6+"$6"} ${7+"$7"} ${8+"$8"} ${9+"$9"} ${10+"$10"} ${11+"$11"} ${12+"$12"} ${13+"$13"} ${14+"$14"} ${15+"$15"} ${16+"$16"} ${17+"$17"} ${18+"$18"} ${19+"$19"}
+$JAVA_HOME ﻿-cp $CLASSPATH -Dfile.encoding=$ENCODING -Xmx1024m -Djava.library.path="linux/32:$PATH" -jar Run.jar ${1+"$1"} ${2+"$2"} ${3+"$3"} ${4+"$4"} ${5+"$5"} ${6+"$6"} ${7+"$7"} ${8+"$8"} ${9+"$9"} ${10+"$10"} ${11+"$11"} ${12+"$12"} ${13+"$13"} ${14+"$14"} ${15+"$15"} ${16+"$16"} ${17+"$17"} ${18+"$18"} ${19+"$19"}
